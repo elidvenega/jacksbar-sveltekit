3687e330b3a19f9d8d52ee90d12488d2df9edbe6
elidvenega <venegaelid@gmail.com>
2023-08-09 18:16:02 -0700

    X added to madal

--- src/lib/layout/components/Modal.svelte
+++ src/lib/layout/components/Modal.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-nocheck
+
 	/**
 	 * @type {any}
 	 */
@@ -11,10 +13,35 @@
 	function handleCloseModal() {
 		closeModal();
 	}
+
+	/**
+	 * @param {any} retVal
+	 */
+	function close(retVal) {
+		// @ts-ignore
+		if (!visible) return;
+		// @ts-ignore
+		window.removeEventListener('keydown', keyPress);
+		// @ts-ignore
+		onTop = prevOnTop;
+		// @ts-ignore
+		if (onTop == null) document.body.style.overflow = '';
+		// @ts-ignore
+		visible = false;
+		// @ts-ignore
+		if (closeCallback) closeCallback(retVal);
+	}
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="modal-overlay" on:click={handleCloseModal}>
+	<div class="modal-top" >
+	<svg id="close" on:click={() => close()} viewBox="0 0 12 12">
+		<circle cx="6" cy="6" r="6" />
+		<line x1="3" y1="3" x2="9" y2="9" />
+		<line x1="9" y1="3" x2="3" y2="9" />
+	</svg>
+	</div>
 	<div class="modal-img">
 		<!-- svelte-ignore a11y-img-redundant-alt -->
 		<img src={imageUrl} alt="Modal Image" />
@@ -24,14 +51,15 @@
 <style>
 	.modal-overlay {
 		position: fixed;
-		top: 0;
+		top: 10%;
 		left: 12%;
-		width: 70%;
-		min-height: 50%;
+		width: 60%;
+		margin: 0 5rem;
 		background-color: rgba(11, 11, 11, 0.6);
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		align-content: center;
 		z-index: 999;
 	}
 
@@ -41,23 +69,61 @@
 		box-shadow: 0 2px 4px rgba(11, 11, 11, 0.2);
 	}
 
+	.modal-top {
+		position: relative;
+	}
+
+	#close {
+		position: absolute;
+		top: -8px;
+		right: -12px;
+		width: 45px;
+		height: 35px;
+		cursor: pointer;
+		fill: #f44;
+		transition: transform 0.3s;
+	}
+
+	#close:hover {
+		transform: scale(2);
+	}
+
+	#close line {
+		stroke: #fff;
+		stroke-width: 2;
+	}
+
 	img {
 		max-width: 100%;
 		height: auto;
 	}
 
-	@media screen and (max-width: 760px) {
+	@media screen and (max-width: 480px) {
 		.modal-overlay {
 			position: fixed;
 			top: 0;
 			left: 0;
 			width: 100%;
+			margin: 0;
 			min-height: 100%;
 			background-color: rgba(11, 11, 11, 0.6);
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			z-index: 999;
+		}
+
+		#close {
+			position: absolute;
+			top: -10rem;
+			bottom: -2rem;
+			/* right: 22rem; */
+			left: 21rem;
+			width: 2.5rem;
+			height: 3rem;
+			cursor: pointer;
+			fill: #f44;
+			transition: transform 0.3s;
 		}
 	}
 </style>

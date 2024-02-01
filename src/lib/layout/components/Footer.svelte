1206ac81579faa60f2535973d7181c0cc5126031
elidvenega <robotsmailNinja@gmail.com>
2024-01-31 23:14:08 -0800

    Updated

--- src/lib/layout/components/Footer.svelte
+++ src/lib/layout/components/Footer.svelte
@@ -1,4 +1,4 @@
 <!-- Footer -->
-<footer class="footer bg-neutral-900 p-11">
-	<h2 class="text-white text-center text-3xl underline">Jacks Bar</h2>
+<footer class="footer bg-neutral-900 p-12">
+	<h2 class="text-white text-center text-4xl underline">Jacks Bar</h2>
 </footer>
